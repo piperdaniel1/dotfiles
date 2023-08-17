a3988196e3fc27342afbfa0389e406489fe6c30c
Daniel Piper <piperdaniel1@gmail.com>
2023-08-17 09:34:43 -0700

    nvim config

--- nvim/lua/piperd/set.lua
+++ nvim/lua/piperd/set.lua
@@ -1,3 +1,6 @@
+local autocmd = vim.api.nvim_create_autocmd
+local augroup = vim.api.nvim_create_augroup
+
 vim.opt.guicursor = ""
 
 vim.opt.nu = true
@@ -7,6 +10,15 @@ vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
+
+augroup('setIndent', { clear = true })
+autocmd('Filetype', {
+  group = 'setIndent',
+  pattern = { 'xml', 'html', 'xhtml', 'css', 'scss', 'javascript', 'typescript',
+    'yaml', 'javascriptreact', 'typescriptreact'
+  },
+  command = 'setlocal shiftwidth=2 tabstop=2'
+})
 
 vim.opt.smartindent = true
 
